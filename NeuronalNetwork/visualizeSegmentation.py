4813b35cfcc4603514c4fd61e5886d708ceffe35
Tobias Lampprecht <tobias.lampprecht@student.reutlingen-university.de>
2018-12-17 11:20:34 +0100

    network and training code

--- NeuronalNetwork/visualizeSegmentation.py
+++ NeuronalNetwork/visualizeSegmentation.py
@@ -33,7 +33,7 @@ fig = plt.figure(figsize=(5,5))
 ax = fig.add_subplot(1,1,1)
 ax.imshow(img_is)
 ax.set_title("original image")
-plt.show()
+# plt.show()
 
 fig = plt.figure(figsize=(15,10))
 for k in range(0,4):
@@ -45,38 +45,38 @@ for k in range(0,4):
     # plt.show()
 
 
-plt.show()
+# plt.show()
 
 
 
 
 
 ## RESIZE IMG
-# import random
-# def give_color_to_seg_img(seg,n_classes):
-#     '''
-#     seg : (input_width,input_height,3)
-#     '''
-#
-#     if len(seg.shape)==3:
-#         seg = seg[:,:,0]
-#     seg_img = np.zeros( (seg.shape[0],seg.shape[1],3) ).astype('float')
-#     colors = sns.color_palette("hls", n_classes)
-#
-#     for c in range(n_classes):
-#         segc = (seg == c)
-#         seg_img[:,:,0] += (segc*( colors[c][0] ))
-#         seg_img[:,:,1] += (segc*( colors[c][1] ))
-#         seg_img[:,:,2] += (segc*( colors[c][2] ))
-#
-#     return(seg_img)
-#
-# input_height , input_width = 224 , 224
-# output_height , output_width = 224 , 224
-#
-#
+import random
+def give_color_to_seg_img(seg,n_classes):
+    '''
+    seg : (input_width,input_height,3)
+    '''
+
+    if len(seg.shape)==3:
+        seg = seg[:,:,0]
+    seg_img = np.zeros( (seg.shape[0],seg.shape[1],3) ).astype('float')
+    colors = sns.color_palette("hls", n_classes)
+
+    for c in range(n_classes):
+        segc = (seg == c)
+        seg_img[:,:,0] += (segc*( colors[c][0] ))
+        seg_img[:,:,1] += (segc*( colors[c][1] ))
+        seg_img[:,:,2] += (segc*( colors[c][2] ))
+
+    return(seg_img)
+
+input_height , input_width = 224 , 224
+output_height , output_width = 224 , 224
+
+
 # ldseg = np.array(os.listdir(dir_seg))
-# for fnm in ldseg[np.random.choice(len(ldseg),3,replace=False)]:
+# for fnm in ldseg[np.random.choice(len(ldseg),2,replace=False)]:
 #     fnm = fnm.split(".")[0]
 #     seg = cv2.imread(dir_seg + fnm + ".png") # (360, 480, 3)
 #     img_is = cv2.imread(dir_img + fnm + ".png")
@@ -97,3 +97,38 @@ plt.show()
 #     ax.imshow(cv2.resize(img_is,(output_height , output_width))/255.0)
 #     ax.set_title("resized to {}".format((output_height , output_width)))
 #     plt.show()
+
+
+def getImageArr( path , width , height ):
+    img = cv2.imread(path, 1)
+    img = np.float32(cv2.resize(img, ( width , height ))) / 127.5 - 1
+    return img
+
+def getSegmentationArr( path , nClasses ,  width , height  ):
+
+    seg_labels = np.zeros((  height , width  , nClasses ))
+    img = cv2.imread(path, 1)
+    img = cv2.resize(img, ( width , height ))
+    img = img[:, : , 0]
+
+    for c in range(nClasses):
+        seg_labels[: , : , c ] = (img == c ).astype(int)
+    ##seg_labels = np.reshape(seg_labels, ( width*height,nClasses  ))
+    return seg_labels
+
+
+
+
+images = os.listdir(dir_img)
+images.sort()
+segmentations  = os.listdir(dir_seg)
+segmentations.sort()
+
+X = []
+Y = []
+for im , seg in zip(images,segmentations) :
+    X.append( getImageArr(dir_img + im , input_width , input_height )  )
+    Y.append( getSegmentationArr( dir_seg + seg , n_classes , output_width , output_height )  )
+
+X, Y = np.array(X) , np.array(Y)
+print(X.shape,Y.shape)
