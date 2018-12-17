f4b92ad377e16a6fdebf388a2010c5b48a8c220b
Tobias Lampprecht <tobias.lampprecht@student.reutlingen-university.de>
2018-12-17 10:06:01 +0100

    added network.py + labels

---
+++ NeuronalNetwork/network.py
@@ -0,0 +1,99 @@
+import cv2, os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2 as cv2
+
+dir_data = "Images/"
+dir_seg = dir_data + "/Labels/"
+dir_img = dir_data + "/Original/"
+
+
+## seaborn has white grid by default so I will get rid of this.
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+
+ldseg = np.array(os.listdir(dir_seg))
+## pick the first image file
+fnm = ldseg[0]
+print(fnm)
+
+## read in the original image and segmentation labels
+seg = cv2.imread(dir_seg + fnm ) # (360, 480, 3)
+img_is = cv2.imread(dir_img + fnm )
+print("seg.shape={}, img_is.shape={}".format(seg.shape,img_is.shape))
+
+## Check the number of labels
+mi, ma = np.min(seg), np.max(seg)
+
+n_classes = ma - mi + 1
+print("minimum seg = {}, maximum seg = {}, Total number of segmentation classes = {}".format(mi,ma, n_classes))
+
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(1,1,1)
+ax.imshow(img_is)
+ax.set_title("original image")
+plt.show()
+
+fig = plt.figure(figsize=(15,10))
+for k in range(0,4):
+    # cv2.imshow("asd",seg)
+    # cv2.waitKey(0);
+    ax = fig.add_subplot(4,n_classes/3,k+1)
+    ax.imshow((seg == k)*1.0)
+    ax.set_title("label = {}".format(k))
+    # plt.show()
+
+
+plt.show()
+
+
+
+
+
+## RESIZE IMG
+# import random
+# def give_color_to_seg_img(seg,n_classes):
+#     '''
+#     seg : (input_width,input_height,3)
+#     '''
+#
+#     if len(seg.shape)==3:
+#         seg = seg[:,:,0]
+#     seg_img = np.zeros( (seg.shape[0],seg.shape[1],3) ).astype('float')
+#     colors = sns.color_palette("hls", n_classes)
+#
+#     for c in range(n_classes):
+#         segc = (seg == c)
+#         seg_img[:,:,0] += (segc*( colors[c][0] ))
+#         seg_img[:,:,1] += (segc*( colors[c][1] ))
+#         seg_img[:,:,2] += (segc*( colors[c][2] ))
+#
+#     return(seg_img)
+#
+# input_height , input_width = 224 , 224
+# output_height , output_width = 224 , 224
+#
+#
+# ldseg = np.array(os.listdir(dir_seg))
+# for fnm in ldseg[np.random.choice(len(ldseg),3,replace=False)]:
+#     fnm = fnm.split(".")[0]
+#     seg = cv2.imread(dir_seg + fnm + ".png") # (360, 480, 3)
+#     img_is = cv2.imread(dir_img + fnm + ".png")
+#     seg_img = give_color_to_seg_img(seg,n_classes)
+#
+#     fig = plt.figure(figsize=(20,40))
+#     ax = fig.add_subplot(1,4,1)
+#     ax.imshow(seg_img)
+#
+#     ax = fig.add_subplot(1,4,2)
+#     ax.imshow(img_is/255.0)
+#     ax.set_title("original image {}".format(img_is.shape[:2]))
+#
+#     ax = fig.add_subplot(1,4,3)
+#     ax.imshow(cv2.resize(seg_img,(input_height , input_width)))
+#
+#     ax = fig.add_subplot(1,4,4)
+#     ax.imshow(cv2.resize(img_is,(output_height , output_width))/255.0)
+#     ax.set_title("resized to {}".format((output_height , output_width)))
+#     plt.show()
